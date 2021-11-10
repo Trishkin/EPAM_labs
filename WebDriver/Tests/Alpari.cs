61e4ca581fd1f4906bbcd7b8e4212e975220e613
Kulikov Dmitry <dima.dmitry.kulikov@gmail.com>
2021-11-10 22:19:35 +0300

    WebDriver 2

--- WebDriver/Tests/Alpari.cs
+++ WebDriver/Tests/Alpari.cs
@@ -7,69 +7,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tests.Pages.AlpariPage;
 
 namespace Tests
 {
     [TestFixture]
     public class Alpari
     {
-        private IWebDriver driver;
-        public string homeURL;
-
+        IWebDriver driver;
 
         [Test]
-        public void ICanWin()
+        public void CreateDemoAccount()
         {
 
-            driver.Manage().Window.Maximize();
-            homeURL = "https://alpari.com/ru/";
-            driver.Navigate().GoToUrl(homeURL);
+            AlpariChromeDriver chromeDriver = new AlpariChromeDriver(driver);
 
-            WebDriverWait wait = new WebDriverWait(driver,
-System.TimeSpan.FromSeconds(15));
-
-            wait.Until(driver =>
-driver.FindElement(By.XPath("//a[@href='/ru/login/']")));
-
-
-            driver.FindElement(By.XPath("//a[@href='/ru/login/']")).Click();
-            wait.Until(driver =>
-driver.FindElement(By.XPath("//*[@id='authorization_login']")));
-            driver.FindElement(By.XPath("//*[@id='authorization_login']")).SendKeys("dima.dmitry.kulikov@gmail.com");
-
-            wait.Until(driver =>
-driver.FindElement(By.XPath("//*[@id='authorization_password']")));
-            driver.FindElement(By.XPath("//*[@id='authorization_password']")).SendKeys("AY%S&iDd9b4c.sg");
-
-            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
-
-            wait.Until(driver =>
-            driver.FindElement(By.XPath("/html/body/div[4]/div[1]/ul/li[2]")));
-            driver.FindElement(By.XPath("/html/body/div[4]/div[1]/ul/li[2]")).Click();
-
-
-            wait.Until(driver =>
-            driver.FindElement(By.XPath("//input[@type='radio'][@value='demo']")));
-            driver.FindElement(By.XPath("//input[@type='radio'][@value='demo']")).Click();
-
-            wait.Until(driver =>
-            driver.FindElement(By.XPath("//input[@type='text'][@value='0.00']")));
-            driver.FindElement(By.XPath("//input[@type='text'][@value='0.00']")).SendKeys(Keys.ArrowLeft+ Keys.ArrowLeft + Keys.ArrowLeft + "100");
-
-
-            driver.FindElement(By.XPath("//button[@type='submit'][@name='send'][@tabindex='0']")).Click();
-
-
-            wait.Until(driver =>
-            driver.FindElement(By.XPath("//input[@type='text'][@value='0.00']")));
-            driver.FindElement(By.XPath("//input[@type='text'][@value='0.00']")).SendKeys("100");
-
-            wait.Until(driver =>
-            driver.FindElement(By.XPath("/html/body/div[4]/div[1]/div[2]/div[2]/div[2]/div[2]/div[1]/ul/li[1]")));
-
-
-            Assert.IsNotNull(driver.FindElement(By.XPath("/html/body/div[4]/div[1]/div[2]/div[2]/div[2]/div[2]/div[1]/ul/li[1]")));
+            chromeDriver.OpenPage();
+            chromeDriver.Login();
+            chromeDriver.OpenAccountPage();
+            chromeDriver.ChooseDemoAndInput100USD();
+            Assert.IsNotNull(chromeDriver.FindAccountInfo());
         }
+
 
         [TearDown]
         public void TearDownTest()
@@ -82,12 +41,11 @@ driver.FindElement(By.XPath("//*[@id='authorization_password']")));
         public void SetupTest()
         {
             ChromeOptions options = new ChromeOptions();
+
             options.AddArguments("load-extension=C:\\Users\\Asus\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\gighmmpiobklfepjocnamgkkbiglidom\\4.39.1_0");
+
             driver = new ChromeDriver(options);
-            homeURL = "https://alpari.com/ru/";
-
         }
-
 
     }
 }
--- WebDriver/Tests/PastebinSeleniumPaste.cs
+++ WebDriver/Tests/PastebinSeleniumPaste.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tests.Pages.AlpariPage;
 
 namespace Tests
 {
@@ -14,85 +15,35 @@ namespace Tests
     public class Chrome_Sample_test
     {
         private IWebDriver driver;
-        public string homeURL;
 
 
         [Test]
-        public void ICanWin()
+        public void CreatePaste()
         {
+            PastebinChromeDriver chromeDriver = new PastebinChromeDriver(driver);
 
-            driver.Manage().Window.Maximize();
-            homeURL = "https://pastebin.com/";
-            driver.Navigate().GoToUrl(homeURL);
-
-            WebDriverWait wait = new WebDriverWait(driver,
-System.TimeSpan.FromSeconds(15));
-
-            wait.Until(driver =>
-driver.FindElement(By.XPath("//textarea[@id='postform-text']")));
-
-
-            driver.FindElement(By.CssSelector("#w0 > div.post-form__bottom > div.post-form__left > div.form-group.field-postform-expiration > div > span")).Click();
-            wait.Until(driver =>
-driver.FindElement(By.XPath("//*[contains(@id,'10M')]")));
-            driver.FindElement(By.XPath("//*[contains(@id,'10M')]")).Click();
-
-            IWebElement element =
-driver.FindElement(By.XPath("//textarea[@id='postform-text']"));
-            element.Click();
-            element.Clear();
-            element.SendKeys("Hello from WebDriver");
-
+            chromeDriver.OpenPage();
+            chromeDriver.Select10MinutesExpiration();
+            chromeDriver.WriteCode("Hello from WebDriver");
+            chromeDriver.WriteTitle("helloweb");
             
-            element =
-driver.FindElement(By.XPath("//input[@id='postform-name']"));
-            element.SendKeys("helloweb");
-
-            element.Submit();
+            Assert.IsNotNull(chromeDriver.Paste());
         }
 
 
 
         [Test]
-        public void BringItOn()
+        public void CreateBashPaste()
         {
+            PastebinChromeDriver chromeDriver = new PastebinChromeDriver(driver);
 
-            driver.Manage().Window.Maximize();
-            homeURL = "https://pastebin.com/";
-            driver.Navigate().GoToUrl(homeURL);
+            chromeDriver.OpenPage();
+            chromeDriver.Select10MinutesExpiration();
+            chromeDriver.SelectBashSintax();
+            chromeDriver.WriteCode("git config --global user.name  \"New Sheriff in Town\" \n git reset $(git commit - tree HEAD ^{ tree} -m \"Legacy code\") \n git push origin master --force");
+            chromeDriver.WriteTitle("how to gain dominance among developers");
 
-            WebDriverWait wait = new WebDriverWait(driver,
-System.TimeSpan.FromSeconds(15));
-
-            wait.Until(driver =>
-driver.FindElement(By.XPath("//textarea[@id='postform-text']")));
-
-            driver.FindElement(By.CssSelector("#w0 > div.post-form__bottom > div.post-form__left > div.form-group.field-postform-expiration > div > span")).Click();
-            wait.Until(driver =>
-driver.FindElement(By.XPath("//*[contains(@id,'10M')]")));
-            driver.FindElement(By.XPath("//*[contains(@id,'10M')]")).Click();
-
-             driver.FindElement(By.CssSelector("#w0 > div.post-form__bottom > div.post-form__left > div.form-group.field-postform-format > div > span")).Click();
-            wait.Until(driver =>
-driver.FindElement(By.XPath("//input[contains(@type,'search')] ")));
-            driver.FindElement(By.XPath("//input[contains(@type,'search')]")).SendKeys("Bash" + Keys.Enter);
-
-
-            IWebElement element =
-driver.FindElement(By.XPath("//textarea[@id='postform-text']"));
-            //element.Click();
-            //element.Clear();
-            element.SendKeys("git config --global user.name  \"New Sheriff in Town\" \n git reset $(git commit - tree HEAD ^{ tree} -m \"Legacy code\") \n git push origin master --force");
-
-            
-
-            element =
-driver.FindElement(By.XPath("//input[@id='postform-name']"));
-            element.SendKeys("how to gain dominance among developers");
-
-
-            element.Submit();
-
+            Assert.IsNotNull(chromeDriver.Paste());
         }
 
 
@@ -107,9 +58,11 @@ driver.FindElement(By.XPath("//input[@id='postform-name']"));
         public void SetupTest()
         {
             ChromeOptions options = new ChromeOptions();
+
+
             options.AddArguments("load-extension=C:\\Users\\Asus\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\gighmmpiobklfepjocnamgkkbiglidom\\4.39.1_0");
+
             driver = new ChromeDriver(options);
-            homeURL = "https://pastebin.com/";
 
         }
 
